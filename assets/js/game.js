18a92303bef2000f91ea10032f3c91d29ae96fb5
jeffmlb91 <jeffmlb91@gmail.com>
2021-08-31 07:41:28 -0400

    add variable and function to define robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("this is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("our Robot's name is" + " " + playerName);
+
+// this creates a function named fight
+
+function fight() {
+  window.alert("The fight has begun");
+}
+//fight();
